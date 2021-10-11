17136e24693536343c5688c949944e384cb9ced7
Mark <61845375+MrMarL@users.noreply.github.com>
2021-10-11 15:00:59 +0300

    lab 1

---
+++ Rabota1/C.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabota1
+{
+    class C:B
+    {
+        public Object aa;
+        public Object aaa;
+    }
+}
---
+++ Rabota1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabota1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            A a2 = new A();
+            A a3 = new A();
+            B b1 = new B();
+            C c1 = new C();
+
+            b1.a = a1;
+            c1.a = b1;
+            c1.aa = a2;
+            c1.aaa = a3;
+        }
+    }
+}
